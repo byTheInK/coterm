da4c51ddaff97c5fd3e806280acb7912ee810550
byTheInK <un.known.river.person@gmail.com>
2025-01-01 21:20:11 +0300

    Fixed script naming bug

--- lang.py
+++ lang.py
@@ -5,9 +5,8 @@ import sys
 def main(file, args, _conf_list):
     module_name = Path(file).stem
     try:
-        sys.path.append(str(Path(file).parent))
-        
-        module = importlib.import_module(module_name)
+        sys.path.append("scripts")
+        module = importlib.import_module("scripts."+module_name)
         
         if not hasattr(module, "main"): raise OSError(f"Error: The module '{module_name}' does not have a main function.")
         if hasattr(module, "args"): module.args = args
@@ -25,5 +24,3 @@ def main(file, args, _conf_list):
         print(ERROR)
     except Exception as e:
         print("ERROR: {}".format(e))
-    finally:
-        sys.path.remove(str(Path(file).parent))
